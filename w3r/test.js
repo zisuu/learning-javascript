20de1885bf33bb88fc62c5e92ef3e70b0f80ff0c
zisuu <david.ziswiler@finecloud.ch>
2023-02-09 19:44:32 +0100

    continue

---
+++ w3r/test.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program to randomize the order of the values of an array, returning a new array.
+
+let input = ["Apple", "Beer", "Dog"]
+
+const randomize = array => {
+    return input
+}
